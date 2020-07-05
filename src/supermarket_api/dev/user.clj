4caa9e5c8dc650d50f607b94b9eb2dab5f1dcfc0
matheusfrancisco <matheusmachadoufsc@gmail.com>
2020-07-05 18:53:31 -0300

    Add repl user to run in hot-reload

---
+++ src/supermarket_api/dev/user.clj
@@ -0,0 +1,33 @@
+(ns supermarket-api.user
+  (:gen-class)
+  (:require [com.stuartsierra.component :as component]
+            [supermarket-api.components.config :as config]
+            [supermarket-api.components.routes :as routes]
+            [supermarket-api.components.storage :as storage]
+            [supermarket-api.components.webserver :as webserver]
+            [supermarket-api.components.datomic :as dt]
+            [supermarket-api.server :as server]
+            [supermarket-api.service :as service]
+            [io.pedestal.service-tools.dev :as dev]
+            [environ.core :refer [env]]))
+
+
+(def system (atom nil))
+
+(defn build-system-map []
+  (component/system-map
+    :config (config/new-config config/config-map)
+    :storage (storage/new-in-memory)
+    :db (dt/new-datomic (env :db-connection-uri))
+    :routes  (routes/new-routes #'supermarket-api.service/routes)
+    :http-server (component/using (webserver/new-webserver) [:config :routes :storage :db])))
+
+(defn main
+  "The entry-point for 'lein run-dev'"
+  [& args]
+  (-> (build-system-map)
+      (server/start-system! system)))
+
+(defn run-dev []
+  (dev/watch) ;; auto-reload namespaces only in run-dev / repl-start
+  (main))
---
+++ src/supermarket_api/logic/user.clj
@@ -0,0 +1,1 @@
+(ns supermarket-api.logic.user)
